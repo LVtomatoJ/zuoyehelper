9894e2e7c3a2cb7c2ea1209d63797115314daf72
LVtomatoj <992203755@qq.com>
2023-04-13 16:51:46 +0800

    add error page or props use

--- web/vue-project/src/views/public/CollectView.vue
+++ web/vue-project/src/views/public/CollectView.vue
@@ -1,19 +1,34 @@
 <script setup>
 import { service } from '../../stores/axios.js'
-import { onBeforeMount } from 'vue'
+import { onMounted,ref } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute();
 const collect_id = route.params.collect_id
-onBeforeMount(() => {
-    service({method:'get',url:'/public/getcollect',params:{collect_id:collect_id}}).then(res=>{
+let collect_exist = ref(null)
+let collect_data = ref(null)
+import Collect from '../../components/public/Collect.vue'
+
+function get_collect(){
+  service({method:'get',url:'/public/getcollect',params:{collect_id:collect_id}}).then(res=>{
         const data = res.data
-        console.log(data)
+        collect_data.value = data
+        collect_exist.value = true
+}).catch((error)=>{
+  // let message = error.response.data.detail
+  collect_exist.value = false
 })
+}
+onMounted(() => {
+  get_collect()
 })
 </script>
 
 <template>
-  <main>
+  <template  v-if="collect_exist===false">
+    <Exception type="404"/>
+  </template>
+  <template v-else-if="collect_exist===true">
     collect {{ collect_id }}
-  </main>
+    <Collect :collect_data='collect_data'></Collect>
+  </template>
 </template>
