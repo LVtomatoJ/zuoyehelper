f254d7b2f695eab80c60a35d4cd07d9c53a73893
LVtomatoj <992203755@qq.com>
2023-04-14 15:37:33 +0800

    添加登出和登录后跳转控制台页面

--- web/vue-project/src/router/index.js
+++ web/vue-project/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import AboutView from '../views/AboutView.vue'
 import CollectView from '../views/public/CollectView.vue'
+import UserIndexView from '../views/user/UserIndexView.vue'
 import { useLoginStore } from '../stores/login'
 
 
@@ -34,6 +35,14 @@ const router = createRouter({
       path:'/public/collect/:collect_id',
       name:'publiccollect',
       component:CollectView
+    },
+    {
+      path: '/user/index',
+      name: 'user_index',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: UserIndexView
     }
 
   ]
--- web/vue-project/src/views/HomeView.vue
+++ web/vue-project/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import { useLoginStore } from '../stores/login'
+const store = useLoginStore()
 const router = useRouter()
 
 
@@ -7,7 +9,10 @@ const handleLogin = () => {
     router.push({path: '/login'})
  }
 
-
+const handleLoginOut=()=>{
+  store.isLoggedIn=false
+  store.jwt=''
+}
 
 </script>
 
@@ -19,13 +24,28 @@ const handleLogin = () => {
         <div style="padding-left: 30px;">作业帮收件箱</div>
       </div>
       </Col>
-      <Col span="10">
+      <Col span="2">
       </Col>
-      <Col span="4">
+      <Col span="12">
       <div style="float: right;">
-        <div style="padding-right: 30px;">
-          <Button @click="handleLogin" type="primary">登录</Button>
-        </div>
+        <template v-if="store.isLoggedIn">
+          <div style="padding-right: 20px;">
+
+                  <Button to="/user/index" type="success" >控制台</Button>
+                  <Divider type="vertical" />
+
+                  <Button @click="handleLoginOut" type="warning">退出</Button>
+
+              
+            
+          </div>
+        </template>
+        <template v-else>
+
+          <div style="padding-right: 30px;">
+            <Button @click="handleLogin" type="primary">登录</Button>
+          </div>
+        </template>
       </div>
       </Col>
     </Row>
