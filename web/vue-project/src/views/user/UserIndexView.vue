fb29e64aeccd444eb237062d9e64923af5571e66
LVtomatoj <992203755@qq.com>
2023-04-18 13:42:29 +0800

    修改判定手机浏览的机制

--- web/vue-project/src/views/user/UserIndexView.vue
+++ web/vue-project/src/views/user/UserIndexView.vue
@@ -107,8 +107,10 @@ console.log(screen_height)
 console.log(screen_width)
 let mobile_flag = false
 //根据屏幕分辨率判断是否是手机
-if (screen_width > 325 && screen_height < 750) {
+// if (screen_width > 325 && screen_height < 750) {
+//     mobile_flag = true;
+// }
+if (screen_width<screen_height) {
     mobile_flag = true;
 }
-
 </script>
